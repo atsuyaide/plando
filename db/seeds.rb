c4a478ae27b773f28f76d5d49e9367a530f459ba
kappa000 <kappa_deatsu@yahoo.co.jp>
2021-09-20 17:15:03 +0000

    test用データを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+N = 3
+M = 10
+
+(1..N).each do |number|
+  number_s = number.to_s
+  user = User.new(name: 'user_' + number_s, email: number_s + '@abc.com', password: 'password')
+  user.save
+end
+  
+(1..N).each do |id|
+  user = User.find(id)
+  # teamを作成
+  (1..M).each do |num|
+    num_s = num.to_s
+    team = user.teams.build(name: user.name + '_' + num_s, description: 'ここにちーむの説明が入ります。' + num_s)
+    team.save
+  end
+end
