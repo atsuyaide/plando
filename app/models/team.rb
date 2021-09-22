1b6fb6f7103fde9d8c2f3d48f44cd5fc2042c355
kappa000 <kappa_deatsu@yahoo.co.jp>
2021-09-22 17:17:39 +0000

    メンバーがいてもチームの削除が可能に

--- app/models/team.rb
+++ app/models/team.rb
@@ -7,7 +7,7 @@ class Team < ApplicationRecord
   
   # teams -> users: 所属メンバー
   has_many :user_teams
-  has_many :members, through: :user_teams, source: :user
+  has_many :members, through: :user_teams, source: :user, dependent: :destroy
   
   def add_member(user)
     self.user_teams.find_or_create_by(user_id: user.id)
